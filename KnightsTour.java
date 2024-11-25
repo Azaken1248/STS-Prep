f632e769b05ecc5877d07e4a303e13622536e0c0
Azaken1248 <suvkrm@gmail.com>
2024-11-26 02:09:38 +0530

    New Tour

--- KnightsTour.java
+++ KnightsTour.java
@@ -1,53 +1,57 @@
 
 public class KnightsTour {
-    static final int N = 8; 
+    public static final int N = 8;
 
-    static final int[] cx = {1, 1, 2, 2, -1, -1, -2, -2};
-    static final int[] cy = {2, -2, 1, -1, 2, -2, 1, -1};
+    public static final int cx[] = {1, 1, 2, 2, -1, -1, -2, -2};
+    public static final int cy[] = {2, -2, 1, -1, 2, -2, 1, -1};
 
-    public static void printBoard(int[][] board) {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                System.out.print(board[i][j] + "\t");
+    public static void printBoard(int board[][], int N){
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                System.out.print(board[i][j] + " ");
             }
             System.out.println();
         }
     }
-
-    public static boolean isSafe(int x, int y, int[][] board) {
+    
+    public static boolean isSafe(int board[][],int N, int x, int y){
         return (x >= 0 && x < N && y >= 0 && y < N && board[x][y] == -1);
     }
 
-    public static int getDegree(int x, int y, int[][] board) {
+    public static int getDegree(int board[][], int N, int x, int y){
         int count = 0;
-        for (int i = 0; i < 8; i++) {
+        for(int i = 0; i < 8; i++){
             int newX = x + cx[i];
             int newY = y + cy[i];
-            if (isSafe(newX, newY, board)) {
+            if(isSafe(board, N, newX, newY)){
                 count++;
             }
         }
+
         return count;
     }
 
-    public static boolean solveKnightsTour(int startX, int startY, int[][] board) {
+    public static boolean solve(int startX, int startY, int board[][], int N){
         board[startX][startY] = 0;
+        
         int moveCount = 1;
 
         int currX = startX;
         int currY = startY;
 
-        while (moveCount < N * N) {
-            int nextX = -1;
-            int nextY = -1;
+        while(moveCount < N * N){
             int minDegree = N + 1;
 
-            for (int i = 0; i < 8; i++) {
+            int nextX = -1;
+            int nextY = -1;
+
+
+            for(int i = 0; i < 8; i++){
                 int newX = currX + cx[i];
                 int newY = currY + cy[i];
-                if (isSafe(newX, newY, board)) {
-                    int degree = getDegree(newX, newY, board);
-                    if (degree < minDegree) {
+                if(isSafe(board, N, newX, newY)){
+                    int degree = getDegree(board, N, newX, newY);
+                    if(degree < minDegree){
                         nextX = newX;
                         nextY = newY;
                         minDegree = degree;
@@ -55,7 +59,7 @@ public class KnightsTour {
                 }
             }
 
-            if (nextX == -1 && nextY == -1) {
+            if(nextX == -1 && nextY == -1){
                 return false;
             }
 
@@ -63,27 +67,26 @@ public class KnightsTour {
             currX = nextX;
             currY = nextY;
             moveCount++;
+
         }
 
-        return true; 
+        return true;
+
     }
 
-    public static void startTour() {
-        int[][] board = new int[N][N];
+    public static void startTour(){
+        int board[][] = new int[N][N];
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
                 board[i][j] = -1;
             }
         }
 
-        int startX = 0;
-        int startY = 0;
-
-        if (solveKnightsTour(startX, startY, board)) {
-            printBoard(board);
-        } else {
-            System.out.println("No solution exists");
+        if(solve(0, 0, board, N)){
+            printBoard(board, N);
+        }else{
+            System.out.println("No Solutions Exist !!");
         }
     }
 
